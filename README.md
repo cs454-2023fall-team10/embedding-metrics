f1b4492c440e77fe43789836101da75fb9766a13
dugi <dugi@channel.io>
2023-11-08 00:40:23 +0900

    feat: prompt engineering and output files

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # embedding-metrics
 
 Attempts to evaluate quality of vector embeddings
+
+## Generate intents powered by OpenAI
+
+```bash
+python3 intent.py <chatbot-filename> <output-filename> <num-intents>
+```
--- intent.py
+++ intent.py
@@ -12,13 +12,12 @@ def generate_intent(graph):
         {
             "role": "system",
             "content": f"""
-Assume that you are a user of a chatbot.
-The chatbot is at the homepage of the Korean IT startup "채널톡".
+You are a user visiting the webpage of the Korean IT startup "채널톡".
 Have conversation with the chatbot.
-Think of a **very** specific situation related to the chatbot's purpose, which can be inferred from name {name}.
-Be creative of a situation that is not too simple.
-Assume korean users, so you should speak in Korean.
-Express your intent in one sentence.
+Think of a **very** specific situation related to the chatbot's name, {name}.
+Be **creative**, but keep the topic related to {name}.
+Express your intent in Korean, in **one** sentence.
+(You don't need to greet or say that you are visiting the homepage. Be brief.)
 """,
         }
     ]
@@ -28,7 +27,7 @@ Express your intent in one sentence.
     assistant_prompt = [
         {
             "role": "user",
-            "content": f"Hello, I am a chatbot {name}. How can I help you?",
+            "content": f"Hello, I am chatbot {name}. How can I help you?",
         }
     ]
     messages.extend(assistant_prompt)
@@ -38,21 +37,39 @@ Express your intent in one sentence.
         model="gpt-3.5-turbo-1106",
         messages=messages,
     )
-    print(response)
 
     # 4. Parse response
     response_message = response.choices[0].message
     return response_message.content
 
 
+def usage():
+    print("Usage: python intent.py <graph_filename> <out_filename> <num_samples>")
+    exit(1)
+
+
 if __name__ == "__main__":
     import sys
+
+    if len(sys.argv) < 4:
+        usage()
+
+    graph_filename = sys.argv[1]
+    out_filename = sys.argv[2]
+    num_samples = int(sys.argv[3])
 
     sys.path.append("chatbot-dataset")
 
     from chatbot import parse_from_file
 
-    chatbot_graph = parse_from_file("chatbot-dataset/examples/jobs-homepage.json")
+    chatbot_graph = parse_from_file(graph_filename)
 
-    intent = generate_intent(chatbot_graph)
-    print(intent)
+    # append
+    with open(out_filename, "a") as f:
+        for i in range(num_samples):
+            print(f"Generating intent {i + 1}/{num_samples}")
+            intent = generate_intent(chatbot_graph)
+            f.write(intent + "\n")
+            f.flush()
+
+    print(f"Generated {num_samples} intents and saved to {out_filename}")
